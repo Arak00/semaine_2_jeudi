af5f4c4c80529087d9f46eca18dd4b6557a95080
Arak00 <31830625+Arak00@users.noreply.github.com>
2018-04-13 00:30:25 +0200

    Update scrapping_cities.rb

--- scrapping_cities.rb
+++ scrapping_cities.rb
@@ -1,32 +1,48 @@
-require 'rubygems'
-require 'nokogiri'
 require 'open-uri'
-
-def get_the_email_of_a_townhal_from_its_webpage
-#On appelle la page à scraper
-page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/vaureal.html'))
-#A partir du css de cette page on vient chercher l'adresse email
-#liée à cette classe
-page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |el|
-   puts el.text
-	end
-end
-
-get_the_email_of_a_townhal_from_its_webpage
-
-
-def get_all_city_names
-#on appelle la page à scraper
-page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-# On
-news_links = page.css(".Style20")
-news_links.each{|link| puts "#{link.text}"}
-end
-get_all_city_names
+require 'nokogiri'
 
 def get_all_the_urls_of_val_doise_townhalls
-	page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-	# On
-	news_links = page.css('/table/tbody/tr[2]/td/table/tbody/tr/td[1]/p/a[1]')
-	news_links.each{|link| puts "#{link.text}"}
+  #ouvre l'URL prête à être scrapée
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+  #créé une liste vide
+  final = Array.new
+  page.css('.a[href]').each do |url|
+    urls = url['href']
+    #ajoute la liste d'URL
+    final.push ("http://annuaire-des-mairies.com#{urls}")
+  end
+  final
 end
+
+
+def get_the_email_of_a_townhal_from_its_webpage(page_url)
+  page = Nokogiri::HTML(open(page_url))
+  puts page.css('tr')[4].css('td')[1]
+end
+#créé une liste composée des urls scrapées
+array_url = get_all_the_urls_of_val_doise_townhalls()
+#retourne la liste d'urls scrapée
+array_url.each do |url|
+  puts get_the_email_of_a_townhal_from_its_webpage(url)
+end
+# On définit une méthode pour scraper les noms
+# de toutes les villes depuis une URL ciblée
+
+def get_all_city_name
+    page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    news_names = page.css("a.lientxt").text
+end
+
+puts get_all_city_name
+
+def get_all_the_urls_of_val_doise_townhalls_2
+  #ouvre l'URL prêtre à être scrapée
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+  #sélectionne les urls dans la boucle
+  page.css(".lientxt[href]").select do |urls|
+  #modifie et remplace le préfixe de l'URL
+  puts urls['href'].gsub('./95/', 'https://annuaire-des-mairies.com/')
+  end
+end
+
+puts get_all_the_urls_of_val_doise_townhalls_2
